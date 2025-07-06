7da17c077c53fb4b15327d39df3463c0a5130345
range79m <darkrange006@gmail.com>
2025-07-06 19:16:43 +0400

    refactor: UserContextUtil refactored, no need for UserRepository anymore
    refactor: getUserId added CustomUserDetails

--- src/main/java/com/range/rpms/common/util/UserContextUtil.java
+++ src/main/java/com/range/rpms/common/util/UserContextUtil.java
@@ -1,5 +1,6 @@
 package com.range.rpms.common.util;
 
+import com.range.rpms.common.security.CustomUserDetails;
 import com.range.rpms.user.dao.repository.UserRepository;
 import com.range.rpms.user.exception.UserNotFoundException;
 import org.springframework.security.core.Authentication;
@@ -8,10 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserContextUtil implements UserContext {
-    private final UserRepository userRepository;
-    public UserContextUtil(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+
+
     @Override
     public String getCurrentUserName() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -23,8 +22,16 @@ public class UserContextUtil implements UserContext {
 
     @Override
     public long getCurrentUserId() {
-   return userRepository.findByUsername(getCurrentUserName()).orElseThrow(()
-           ->new UserNotFoundException("user id not found")).getId();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new UserNotFoundException("No authenticated user found");
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomUserDetails customUserDetails) {
+            return customUserDetails.getId();
+        }
+        throw new UserNotFoundException("Invalid user principal");
     }
+
 
 }
