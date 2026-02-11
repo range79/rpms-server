81bef3ae2e4faafb8f810f4aac0f1e0e5ae3d644
range79 <darkrange006@gmail.com>
2026-02-12 02:56:27 +0400

    kotln plugin added

---
+++ src/main/java/com/range/rpms/projects/domain/ProjectEntity.kt
@@ -0,0 +1,4 @@
+package com.range.rpms.projects.domain
+
+class ProjectEntity {
+}
--- src/main/java/com/range/rpms/users/enums/Role.java
+++ src/main/java/com/range/rpms/users/enums/Role.java
@@ -5,8 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
 
     ROLE_USER,
-    ROLE_ADMIN,
-    ;
+    ROLE_ADMIN,;
 
 
     @Override
