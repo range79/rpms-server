e51ea2015e840f4153b30338fb45cf567a252446
range79 <darkrange006@gmail.com>
2026-02-20 19:40:12 +0400

    authServiceImpl added

--- src/main/kotlin/com/example/rpms/users/service/AuthService.kt
+++ src/main/kotlin/com/example/rpms/users/service/AuthService.kt
@@ -2,8 +2,14 @@ package com.example.rpms.users.service
 
 import com.example.rpms.users.dto.AuthResponse
 import com.example.rpms.users.dto.LoginRequest
+import com.example.rpms.users.dto.RegisterRequest
 
 interface AuthService {
     fun login(loginRequest: LoginRequest): AuthResponse
-    fun register(registerRequest:RegisterRequest)
+    fun register(registerRequest: RegisterRequest)
+    fun resetPassword(token: String)
+    fun forgotPassword(email: String)
+    fun twoFactoryAuthRequest(email:String)
+    fun acceptTwoFactoryAuthRequest(token: String):AuthResponse
+    fun logout()
 }
