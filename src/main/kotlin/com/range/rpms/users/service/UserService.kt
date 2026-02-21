1ad9b8ca02a25950fb28e365c034385dd02f608e
range79 <darkrange006@gmail.com>
2026-02-21 20:24:38 +0400

    user service test added

--- src/main/kotlin/com/range/rpms/users/service/UserService.kt
+++ src/main/kotlin/com/range/rpms/users/service/UserService.kt
@@ -5,5 +5,5 @@ import com.range.rpms.users.domain.entity.User
 interface UserService {
     fun findByUsername(username: String): User?
     fun findByEmail(email: String): User?
-    fun findByEmailORUsername(emailOrUsername: String): User?
+    fun findByEmailORUsername(emailORUsername: String): User?
 }
