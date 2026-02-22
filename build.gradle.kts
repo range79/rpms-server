335c4788d5daca3f1cf93b941b8c1ff15a875ecc
range79 <darkrange006@gmail.com>
2026-02-22 23:28:34 +0400

    swagger ui dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
