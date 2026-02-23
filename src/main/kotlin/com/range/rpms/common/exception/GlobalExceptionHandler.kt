d6d0e769636bcd2629ad743c005f41a7e702f6cf
range79 <darkrange006@gmail.com>
2026-02-24 01:01:43 +0400

    validation handler added

--- src/main/kotlin/com/range/rpms/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/range/rpms/common/exception/GlobalExceptionHandler.kt
@@ -4,6 +4,7 @@ import com.range.rpms.common.dto.ExceptionResponse
 import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -22,6 +23,26 @@ class GlobalExceptionHandler{
             ex.localizedMessage,
             request.requestURI,
             ex.status
+        )
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationException(
+        ex: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ExceptionResponse {
+
+        val firstError = ex.bindingResult
+            .fieldErrors
+            .firstOrNull()
+
+        val message = firstError?.defaultMessage ?: "Validation failed"
+
+        return ExceptionResponse(
+            message,
+            request.requestURI,
+            HttpStatus.BAD_REQUEST
         )
     }
     @ExceptionHandler(Exception::class)
