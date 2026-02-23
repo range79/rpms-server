4d4b181142898f62e51af4a7ed76cc7f722c8fd1
range79 <darkrange006@gmail.com>
2026-02-23 22:00:44 +0400

    regiter and login implemented

--- src/main/kotlin/com/range/rpms/users/service/AuthService.kt
+++ src/main/kotlin/com/range/rpms/users/service/AuthService.kt
@@ -6,7 +6,7 @@ import com.range.rpms.users.dto.RegisterRequest
 
 interface AuthService {
     fun login(loginRequest: LoginRequest): AuthResponse
-    fun register(registerRequest: RegisterRequest)
+    fun register(registerRequest: RegisterRequest): AuthResponse
     fun resetPassword(token: String)
     fun forgotPassword(email: String)
     fun twoFactoryAuthRequest(email:String)
--- src/main/kotlin/com/range/rpms/users/service/impl/AuthServiceImpl.kt
+++ src/main/kotlin/com/range/rpms/users/service/impl/AuthServiceImpl.kt
@@ -1,33 +1,44 @@
 package com.range.rpms.users.service.impl
 
+import com.range.rpms.tokens.tokenfactory.TokenFactory
 import com.range.rpms.users.dto.AuthResponse
 import com.range.rpms.users.dto.LoginRequest
 import com.range.rpms.users.dto.RegisterRequest
 import com.range.rpms.users.service.AuthService
+import com.range.rpms.users.service.LoginService
+import com.range.rpms.users.service.PasswordService
+import com.range.rpms.users.service.RegisterService
 import org.springframework.stereotype.Service
 
 @Service
-class AuthServiceImpl (
+class AuthServiceImpl(
+    val tokenFactory: TokenFactory,
+    val loginService: LoginService,
+    val registerService: RegisterService,
+    val passwordService: PasswordService
+)
 
-):AuthService {
+:AuthService {
     override fun login(loginRequest: LoginRequest):AuthResponse {
-        TODO("Not yet implemented")
+        val user =loginService.login(loginRequest)
+        return tokenFactory.generateTokens(user)
     }
 
-    override fun register(registerRequest: RegisterRequest) {
-        TODO("Not yet implemented")
+    override fun register(registerRequest: RegisterRequest) : AuthResponse{
+        val user =registerService.register(registerRequest)
+        return tokenFactory.generateTokens(user)
     }
 
     override fun resetPassword(token: String) {
-        TODO("Not yet implemented")
+        TODO()
     }
 
     override fun forgotPassword(email: String) {
-        TODO("Not yet implemented")
+      passwordService.sendPasswordResetEmail(email)
     }
 
     override fun twoFactoryAuthRequest(email: String) {
-        TODO("Not yet implemented")
+
     }
 
     override fun acceptTwoFactoryAuthRequest(token: String):AuthResponse {
