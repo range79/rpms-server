7a81bc0aa3c85d46274271a50c3e71ecd5ca7d34
range79 <darkrange006@gmail.com>
2026-02-24 00:59:27 +0400

    validation addded

--- src/main/kotlin/com/range/rpms/users/api/AuthApi.kt
+++ src/main/kotlin/com/range/rpms/users/api/AuthApi.kt
@@ -4,6 +4,7 @@ import com.range.rpms.users.dto.AuthResponse
 import com.range.rpms.users.dto.ForgotPasswordRequest
 import com.range.rpms.users.dto.LoginRequest
 import com.range.rpms.users.dto.RegisterRequest
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam
 @RequestMapping("/v1/api/auth")
 interface AuthApi {
     @PostMapping("/login")
-    fun login(@RequestBody loginRequest: LoginRequest): AuthResponse
+    fun login(@RequestBody @Valid loginRequest: LoginRequest): AuthResponse
 
     @PostMapping("/register")
-    fun register(@RequestBody registerRequest: RegisterRequest): AuthResponse
+    fun register(@RequestBody @Valid registerRequest: RegisterRequest): AuthResponse
 
     @PostMapping("/forgot-password")
-    fun forgotPassword(@RequestBody forgotPasswordRequest: ForgotPasswordRequest)
+    fun forgotPassword(@RequestBody @Valid forgotPasswordRequest: ForgotPasswordRequest)
 
 
 }
